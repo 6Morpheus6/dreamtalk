e6da461f78f87bcda16c23bdd5d0c0f92e8de4d8
6Morpheus6 <wanton-decay@gmx.net>
2026-03-26 21:51:54 +0100

    update
    - remove quotes

--- start.js
+++ start.js
@@ -1,17 +1,23 @@
 module.exports = {
+  requires: {
+    bundle: "ai",
+  },
   daemon: true,
   run: [{
-    "method": "shell.run",
-    "params": {
-      "venv": "env",
-      "path": "app",
-      "message": "python app.py",
-      "on": [{ "event": "/http:\/\/[0-9.:]+/", "done": true }]
+    method: "shell.run",
+    params: {
+      venv: "env",
+      path: "app",
+      message: "python app.py",
+      on: [{
+        "event": "/http:\/\/[0-9.:]+/",
+        "done": true
+      }]
     }
   }, {
-    "method": "local.set",
-    "params": {
-      "url": "{{input.event[0]}}"
+    method: "local.set",
+    params: {
+      url: "{{input.event[0]}}"
     }
   }]
 }
